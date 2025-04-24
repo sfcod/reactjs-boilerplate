f7b617fd14f3a90551d02de67ebbc7c237b89254
yevhen.drobaniuk <drobaniuk@gmail.com>
2025-04-24 22:10:49 +0300

    init rtk query

---
+++ src/store/api/user.ts
@@ -0,0 +1,34 @@
+import type { ListUsersQueryParams, UpdateUserData, UpdateUserResultData, User } from 'src/types/user';
+import { createTagProvider, paginatedTransformer } from '../../helpers/store';
+import { api } from './';
+import type { Paginated } from '../../types/common';
+import { prepareQueryParams } from '../../helpers/axios';
+
+const userApi = api.enhanceEndpoints({ addTagTypes: ['User', 'Users'] }).injectEndpoints({
+    endpoints: (builder) => ({
+        getUsers: builder.query<Paginated<User>, ListUsersQueryParams>({
+            query: (params) => ({
+                url: `/users`,
+                params: prepareQueryParams(params),
+            }),
+            providesTags: createTagProvider('Users', 'User'),
+            transformResponse: paginatedTransformer,
+        }),
+
+        getUser: builder.query<User, string>({
+            query: (id) => `/users/${id}`,
+            providesTags: createTagProvider('User'),
+        }),
+
+        editUser: builder.mutation<UpdateUserResultData, UpdateUserData & Pick<User, 'id'>>({
+            query: ({ id, ...data }) => ({
+                url: `/users/${id}`,
+                method: 'PATCH',
+                body: data,
+            }),
+            invalidatesTags: createTagProvider('User'),
+        }),
+    }),
+});
+
+export const { useGetUserQuery, useGetUsersQuery, useEditUserMutation } = userApi;
---
+++ src/store/index.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { api } from './api';
+
+export const store = configureStore({
+    reducer: {
+        [api.reducerPath]: api.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+});
+
+export type StoreState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
